29b159d98c6b9114174954b96ea08bc9d275a46b
bbflower <sayeefm0@gmail.com>
2022-06-20 20:11:30 -0700

    small things; writeroom with multiple buffers works

--- emacs/lisp/writeroom-setup.el
+++ emacs/lisp/writeroom-setup.el
@@ -1,7 +1,9 @@
 (use-package writeroom-mode
-  :hook (prog-mode)
+  :hook (prog-mode text-mode org-mode)
   :bind
   (:map usr-map/view
-        ("w" . writeroom-mode)))
+        ("w" . writeroom-mode))
+  :config
+  (setq writeroom-maximize-window nil))
 
 (provide 'writeroom-setup)
